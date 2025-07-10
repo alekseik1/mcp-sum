299dcc99b7b12de9f46303ab5190db66518997d0
alekseik1 <1alekseik1@gmail.com>
2025-07-10 20:15:52 +0300

    feat: working MCP

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-import re
+from mcp.server.fastmcp import FastMCP
 
-from fastapi import FastAPI
-
-from schemas import MCPRequest, MCPResponse, Segment
-
-app = FastAPI()
+mcp = FastMCP("Simple MCP Server")
 
 
-@app.post("/mcp", response_model=MCPResponse)
-async def handle_mcp(request: MCPRequest):
-    # Получаем последнее сообщение пользователя
-    last_user_message = next(
-        (
-            seg.content
-            for seg in reversed(request.context.segments)
-            if seg.role == "user"
-        ),
-        "",
-    )
+@mcp.tool()
+def sum_two(a: int, b: int) -> int:
+    """
+    Sums two integers.
+    """
+    print("ХУЙ ПИЗДА СКОВОРОДА")
+    return a + b
 
-    # Парсим два числа из текста (очень просто)
-    numbers = list(map(int, re.findall(r"\d+", last_user_message)))
-    if len(numbers) >= 2:
-        result = numbers[0] + numbers[1]
-        answer = f"Результат: {result}"
-    else:
-        answer = "Я не смог найти два числа для сложения."
 
-    return MCPResponse(segment=Segment(role="assistant", content=answer))
+if __name__ == "__main__":
+    mcp.run("sse")
